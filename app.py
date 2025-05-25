1434d5a439019f9ed9eeb97967450af19ffc42b0
Robotech <online.amitdhiman@gmail.com>
2025-05-25 21:42:50 +0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ DB_NAME = os.getenv("POSTGRES_DB")
 # Connection string
 #  DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 # postgresql://[user]:[password]@yg0c48okc8skscco0wkws0gc:5432/[database] 
-DATABASE_URL = "postgresql://postgres:QjRfozIQ4xzQj1oiOUIyvDSrgNrFCXwZosvgFOXVKmhO4h7uBbYqKfXhbGpNq4pI@10.0.1.7:5432/postgres"
+DATABASE_URL = "postgresql://postgres:QjRfozIQ4xzQj1oiOUIyvDSrgNrFCXwZosvgFOXVKmhO4h7uBbYqKfXhbGpNq4pI@jc80c4ow8kw04gcg8c4kcc4g:5432/postgres"
 # SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
 
